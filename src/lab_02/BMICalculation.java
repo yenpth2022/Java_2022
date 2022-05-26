c20521cf2e3062d28a1fc4360d5fd3d1a0a2b11c
Yen Pham <yenpth2022@gmail.com>
2022-05-26 23:05:36 +0700

    update lab 2

--- src/lab_02/BMICalculation.java
+++ src/lab_02/BMICalculation.java
@@ -16,12 +16,16 @@ public class BMICalculation {
         float BMI = weight/((height/100)*2);
         if (BMI <= 18.5){
             System.out.println("Underweight");
+            System.out.println("You should increase your weight");
         } else if (BMI <= 24.9){
             System.out.println("Normal weight");
+            System.out.println("You are beautiful ^^");
         } else if (BMI <= 29.9) {
             System.out.println("Overweight");
+            System.out.println("You should decrease your weight");
         } else {
             System.out.println("Obesity");
+            System.out.println("Decrease your weight immediately");
         }
 
     }
---
+++ src/lab_02/NumberDefine.java
@@ -0,0 +1,19 @@
+package lab_02;
+
+import java.util.Scanner;
+
+public class NumberDefine {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your number:");
+        int inputNumber = scanner.nextInt();
+
+        if(inputNumber % 2 == 0){
+            System.out.println("Your number is even number");
+        } else {
+            System.out.println("Your number is odd number");
+        }
+    }
+}
